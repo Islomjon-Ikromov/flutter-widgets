97ebefcce0707b35e2458f16033cb02b0ab9677b
islomjon <mcxacker7@gmail.com>
2024-02-12 18:00:16 +0500

    add sliver app bar

---
+++ lib/flutter/widgets/appbar/sliver_appbars.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+// wrap with CustomScrollView in slivers
+
+// after appbar put
+// SliverList(
+//             delegate: SliverChildBuilderDelegate(
+//               (BuildContext context, int index) {
+//                 return Text("$index");
+//               },
+//               childCount: 20,
+//             ),
+//           ),
+class MySliverAppBar extends StatelessWidget {
+  const MySliverAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      // flexibleSpace: Image.network("https://w.forfun.com/fetch/64/648ceff8c4e804efda307e71a08208b9.jpeg"),
+      leading: IconButton(
+        onPressed: () {},
+        icon: const Icon(
+          Icons.menu,
+          color: Colors.white,
+          size: 30,
+        ),
+      ),
+      // title: const Text(
+      //   "T I T L E",
+      //   style: TextStyle(
+      //     color: Colors.white,
+      //     fontWeight: FontWeight.w600,
+      //   ),
+      // ),
+      centerTitle: true,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.search,
+            color: Colors.white,
+            size: 30,
+          ),
+        ),
+      ],
+      expandedHeight: 300,
+
+      backgroundColor: Colors.blue,
+      elevation: 1,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Container(
+          color: Colors.green,
+        ),
+        title: const Text(
+          "T I T L E",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+        // background: Image.network(
+        //   "https://w.forfun.com/fetch/64/648ceff8c4e804efda307e71a08208b9.jpeg",
+        // ),
+      ),
+      pinned: true,
+      floating: false,
+      snap: false,
+    );
+  }
+}
