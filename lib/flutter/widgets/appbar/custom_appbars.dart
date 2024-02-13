5da6662414b331b6b13dfeb653f50171a52bc007
islomjon <mcxacker7@gmail.com>
2024-02-13 09:56:44 +0500

    add new app bar

--- lib/flutter/widgets/appbar/custom_appbars.dart
+++ lib/flutter/widgets/appbar/custom_appbars.dart
@@ -140,3 +140,57 @@ class CustomAppBar2 extends StatelessWidget {
     );
   }
 }
+
+// ============================== MyCustomAppBar3 ==============================
+
+// appBar: CustomAppBar3(),
+// drawer: null
+
+class CustomAppBar3 extends StatelessWidget implements PreferredSize {
+  const CustomAppBar3({super.key});
+
+  @override
+  Widget build(BuildContext appBarContext) {
+    return AppBar(
+      backgroundColor: Colors.blue,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          bottomRight: Radius.circular(10),
+        ),
+      ),
+      title: const Text(
+        "T I T L E",
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w600,
+          fontSize: 25,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      centerTitle: false,
+      actions: [
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: IconButton(
+            padding: EdgeInsets.zero,
+            alignment: Alignment.center,
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  // TODO: implement child
+  Widget get child => const Text("");
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60);
+}
