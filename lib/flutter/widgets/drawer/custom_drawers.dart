e6e2c7f90db55985c183f6b0ac5ab5e1bee4a035
islomjon <mcxacker7@gmail.com>
2024-02-13 13:50:40 +0500

    add new drawer

--- lib/flutter/widgets/drawer/custom_drawers.dart
+++ lib/flutter/widgets/drawer/custom_drawers.dart
@@ -10,6 +10,13 @@ class CustomDrawer1 extends Drawer {
   CustomDrawer1({super.key, required this.drawerContext})
       : super(
           // width: 300,
+          backgroundColor: Colors.white,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+          ),
           child: ListView(
             padding: EdgeInsets.zero,
             children: [
@@ -82,6 +89,101 @@ class CustomDrawer1 extends Drawer {
                 onTap: () {
                   Navigator.of(drawerContext).pop();
                 },
+              ),
+            ],
+          ),
+        );
+}
+
+// ============================== CustomDrawer2 ==============================
+
+// drawer: CustomDrawer2()
+
+class CustomDrawer2 extends Drawer {
+  BuildContext drawerContext;
+
+  CustomDrawer2({super.key, required this.drawerContext})
+      : super(
+          // width: 300,
+          backgroundColor: Colors.white,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+          ),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              Material(
+                color: Colors.blueAccent,
+                child: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: EdgeInsets.only(
+                      top: MediaQuery.of(drawerContext).padding.top,
+                      bottom: 24,
+                    ),
+                    child: const Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 52,
+                          backgroundImage: NetworkImage(
+                            'https://ikramov.uz/assets/img/avatar1.jpg',
+                          ),
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Text(
+                          'Islomjon Ikromov',
+                          style: TextStyle(fontSize: 28, color: Colors.white),
+                        ),
+                        Text(
+                          'ikramov.uz',
+                          style: TextStyle(fontSize: 14, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.home_outlined),
+                    title: const Text('Home'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.favorite_border),
+                    title: const Text('Favourites'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.workspaces),
+                    title: const Text('Workflow'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.update),
+                    title: const Text('Updates'),
+                    onTap: () {},
+                  ),
+                  const Divider(
+                    color: Colors.black45,
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.account_tree_outlined),
+                    title: const Text('Plugins'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.notifications_outlined),
+                    title: const Text('Notifications'),
+                    onTap: () {},
+                  ),
+                ],
               ),
             ],
           ),
