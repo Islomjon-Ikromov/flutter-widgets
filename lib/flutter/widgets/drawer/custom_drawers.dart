bf9830528c43b8a14b0a5ec32a45696d06fe1604
islomjon <mcxacker7@gmail.com>
2024-02-13 10:07:17 +0500

    update drawer

--- lib/flutter/widgets/drawer/custom_drawers.dart
+++ lib/flutter/widgets/drawer/custom_drawers.dart
@@ -4,88 +4,86 @@ import 'package:flutter/material.dart';
 
 // drawer: CustomDrawer1()
 
-class CustomDrawer1 extends StatelessWidget {
-  const CustomDrawer1({super.key});
+class CustomDrawer1 extends Drawer {
+  BuildContext drawerContext;
 
-  @override
-  Widget build(BuildContext drawerContext) {
-    return Drawer(
-      // width: 300,
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              image: DecorationImage(
-                image: NetworkImage(
-                  "https://outsider.ie/wp-content/uploads/2022/02/Outsider.ie-Dolomites.jpg",
+  CustomDrawer1({super.key, required this.drawerContext})
+      : super(
+          // width: 300,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://outsider.ie/wp-content/uploads/2022/02/Outsider.ie-Dolomites.jpg",
+                    ),
+                    fit: BoxFit.fill,
+                  ),
                 ),
-                fit: BoxFit.fill,
+                child: Text(
+                  'Your Profile',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                  ),
+                ),
               ),
-            ),
-            child: Text(
-              'Your Profile',
-              style: TextStyle(
-                color: Colors.red,
-                fontWeight: FontWeight.w600,
-                fontSize: 18,
+              ListTile(
+                leading: const Icon(
+                  Icons.home,
+                  color: Colors.black,
+                ),
+                title: const Text(
+                  'Home',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
               ),
-            ),
+              ListTile(
+                leading: const Icon(
+                  Icons.person,
+                  color: Colors.black,
+                ),
+                title: const Text(
+                  'Profile',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              const Divider(),
+              ListTile(
+                leading: const Icon(
+                  Icons.settings,
+                  color: Colors.black,
+                ),
+                title: const Text(
+                  'Settings',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                onTap: () {
+                  Navigator.of(drawerContext).pop();
+                },
+              ),
+            ],
           ),
-          ListTile(
-            leading: const Icon(
-              Icons.home,
-              color: Colors.black,
-            ),
-            title: const Text(
-              'Home',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            onTap: () {
-              // Update the state of the app.
-              // ...
-            },
-          ),
-          ListTile(
-            leading: const Icon(
-              Icons.person,
-              color: Colors.black,
-            ),
-            title: const Text(
-              'Profile',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            onTap: () {
-              // Update the state of the app.
-              // ...
-            },
-          ),
-          const Divider(),
-          ListTile(
-            leading: const Icon(
-              Icons.settings,
-              color: Colors.black,
-            ),
-            title: const Text(
-              'Settings',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            onTap: () {
-              Navigator.of(drawerContext).pop();
-            },
-          ),
-        ],
-      ),
-    );
-  }
+        );
 }
