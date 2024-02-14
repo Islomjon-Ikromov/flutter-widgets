70f824a7442c029aeba603f58038d4aafc354eb5
islomjon <mcxacker7@gmail.com>
2024-02-14 15:45:47 +0500

    add new animated bottom navigation bar

--- lib/flutter/widgets/bottom_navigation_bar/custom_animated_bottom_navigation_bars.dart
+++ lib/flutter/widgets/bottom_navigation_bar/custom_animated_bottom_navigation_bars.dart
@@ -105,15 +105,7 @@ class _CustomAnimatedBottomNavigationBar1State
 
 // ============================== CustomAnimatedBottomNavigationBar2 ==============================
 
-// Stack(
-//   children: [
-//     other_widgets
-//     const Align(
-//       alignment: Alignment.bottomCenter,
-//       child: CustomAnimatedBottomNavigationBar2(),
-//     ),
-//   ],
-// ),
+// bottomNavigationBar: const CustomAnimatedBottomNavigationBar2(),
 
 class CustomAnimatedBottomNavigationBar2 extends StatefulWidget {
   const CustomAnimatedBottomNavigationBar2({Key? key}) : super(key: key);
@@ -133,14 +125,14 @@ class _CustomAnimatedBottomNavigationBar2State
       height: 60,
       width: double.infinity,
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      margin: const EdgeInsets.only(bottom: 30, left: 10, right: 10),
+      // margin: const EdgeInsets.only(bottom: 30, left: 10, right: 10),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: const BorderRadius.only(
-          bottomLeft: Radius.circular(30),
-          bottomRight: Radius.circular(30),
-          topLeft: Radius.circular(30),
-          topRight: Radius.circular(30),
+          bottomLeft: Radius.circular(0),
+          bottomRight: Radius.circular(0),
+          topLeft: Radius.circular(0),
+          topRight: Radius.circular(0),
         ),
         boxShadow: [
           BoxShadow(
@@ -196,6 +188,114 @@ class _CustomAnimatedBottomNavigationBar2State
             icon,
             size: 30,
             color: index == currentIndex ? Colors.blueAccent : Colors.black38,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Stack(
+//   children: [
+//     other_widgets
+//     const Align(
+//       alignment: Alignment.bottomCenter,
+//       child: CustomAnimatedBottomNavigationBar3(),
+//     ),
+//   ],
+// ),
+//
+// or
+//
+// bottomNavigationBar: const CustomAnimatedBottomNavigationBar3(),
+
+class CustomAnimatedBottomNavigationBar3 extends StatefulWidget {
+  const CustomAnimatedBottomNavigationBar3({super.key});
+
+  @override
+  State<CustomAnimatedBottomNavigationBar3> createState() =>
+      _CustomAnimatedBottomNavigationBar3State();
+}
+
+class _CustomAnimatedBottomNavigationBar3State
+    extends State<CustomAnimatedBottomNavigationBar3> {
+  int currentIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      margin: const EdgeInsets.only(bottom: 10, left: 10, right: 10),
+      decoration: BoxDecoration(
+        color: Color(0xFF948af8),
+        borderRadius: const BorderRadius.all(Radius.circular(30)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          buildNavItem(1, Icons.home, Icons.home_outlined),
+          // buildNavItem(2, Icons.search, Icons.search),
+          buildNavItem(3, Icons.favorite, Icons.favorite_outline),
+          buildNavItem(4, Icons.settings, Icons.settings_outlined),
+        ],
+      ),
+    );
+  }
+
+  Widget buildNavItem(int index, IconData icon1, IconData icon2) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // AnimatedContainer(
+          //   duration: const Duration(milliseconds: 2000),
+          //   curve: Curves.fastLinearToSlowEaseIn,
+          //   margin: const EdgeInsets.only(
+          //     bottom: 10,
+          //     right: 10,
+          //     left: 10,
+          //   ),
+          //   width: 50,
+          //   height: 5,
+          //   decoration: BoxDecoration(
+          //     color: index == currentIndex ? Colors.white : Colors.white,
+          //     borderRadius: const BorderRadius.vertical(
+          //       bottom: Radius.circular(30),
+          //       top: Radius.circular(0),
+          //     ),
+          //   ),
+          // ),
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 2000),
+            curve: Curves.fastLinearToSlowEaseIn,
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+            decoration: BoxDecoration(
+              color: index == currentIndex ? Color(0xFF6054e8) : Color(0xFF948af8),
+              borderRadius: BorderRadius.all(
+                Radius.circular(30),
+              ),
+            ),
+            child: Icon(
+              index == currentIndex ? icon1 : icon2,
+              size: 25,
+              color: Colors.white,
+            ),
           ),
         ],
       ),
